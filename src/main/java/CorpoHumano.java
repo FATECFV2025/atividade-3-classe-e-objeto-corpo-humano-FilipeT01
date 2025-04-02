48a733b892c4a19063111929ff796799e33421a0
FilipeT01 <filipe.torres01@fatec.sp.gov.br>
2025-04-02 12:22:28 -0300

    atividade

--- src/main/java/CorpoHumano.java
+++ src/main/java/CorpoHumano.java
@@ -1,5 +1,56 @@
 package main.java;
-
+ 
 public class CorpoHumano {
-
+   
+    private float massa;
+    private float volume;
+    private float densidade;
+    private float altura;
+ 
+   
+    public CorpoHumano(float massa, float volume, float densidade, float altura) {
+        this.massa = massa;
+        this.volume = volume;
+        this.densidade = densidade;
+        this.altura = altura;
+    }
+ 
+    // Métodos Getters
+    public float getMassa() {
+        return massa;
+    }
+ 
+    public float getVolume() {
+        return volume;
+    }
+ 
+    public float getDensidade() {
+        return densidade;
+    }
+ 
+    public float getAltura() {
+        return altura;
+    }
+ 
+    // Métodos Setters
+    public void setMassa(float massa) {
+        this.massa = massa;
+    }
+ 
+    public void setVolume(float volume) {
+        this.volume = volume;
+    }
+ 
+    public void setDensidade(float densidade) {
+        this.densidade = densidade;
+    }
+ 
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+ 
+   
+    public float calcularIMC() {
+        return massa / (altura * altura);
+    }
 }
